14bfd0d4d601892df0ee898a1d355ddd62ccc03f
sk1981 <sinavasi@gmail.com>
2016-06-20 05:02:04 +0530

    1. Added basic sticky header

---
+++ src/StickyHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header.js';
+
+const StickyHeader = (props) => {
+  return (
+    <div className="sticky-header">
+      <Header>{props.children}</Header>
+    </div>
+  );
+};
+
+export default StickyHeader;
