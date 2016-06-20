d1d1c134bc0730acd7e52ecfdcf4ff8960aa7146
sk1981 <sinavasi@gmail.com>
2016-06-20 23:51:37 +0530

    Initial template of examples

---
+++ examples/README.md
@@ -0,0 +1,7 @@
+#Examples
+
+* Basic Header Example - Show a basic header with default settings
+* Header with theme - Show a header with the sass variables changed to provide a different theme.
+* Header on Scroll Up - Header which only shows on scroll up
+* Sticky Header - header which sticks to the top 
+
---
+++ examples/sticky-scrollup-header/index.js
@@ -0,0 +1,27 @@
+import StickyOnScrollUpHeader from './StickyOnScrollUpHeader.js'
+import React from 'react';
+import Logo from './Logo.js';
+import NavigationBar from './NavigationBar.js';
+import NavigationItem from './NavigationItem.js';
+
+const header = (
+  <StickyOnScrollUpHeader>
+    <Logo title="My Logo" link="http://testing.com"></Logo>
+    <NavigationBar>
+      <NavigationItem link="#" text="Home"></NavigationItem>
+      <NavigationItem text="Team">
+        <NavigationBar>
+          <NavigationItem link="#" text="Local"></NavigationItem>
+          <NavigationItem link="#" text="Remote"></NavigationItem>
+          <NavigationItem link="#" text="Desktop"></NavigationItem>
+          <NavigationItem link="#" text="Train"></NavigationItem>
+        </NavigationBar>
+      </NavigationItem>
+      <NavigationItem link="#" text="Contact"></NavigationItem>
+      <NavigationItem link="#" text="About Us"></NavigationItem>
+    </NavigationBar>
+  </StickyOnScrollUpHeader>
+);
+
+render(header, document.getElementById('header'));
+
