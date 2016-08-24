9efc41a1f04ed25ce9c62eb68b38ebb2f45a6d4e
sk1981 <sinavasi@gmail.com>
2016-08-24 23:20:22 +0530

    Added main file for exporting usesable components

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Header from './Header.js';
+import Logo from './Logo.js';
+import NavigationList from './navigation/NavigationList';
+import NavigationItem from './navigation/NavigationItem';
+
+export default {
+  Header: Header,
+  Logo: Logo,
+  NavigationList: NavigationList,
+  NavigationItem: NavigationItem
+};
+
+export {Header};
+export {Logo};
+export {NavigationList};
+export {NavigationItem};
