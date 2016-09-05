f4147e7cc2f8e336238421b474d77ef4f3c1eff0
sk1981 <sinavasi@gmail.com>
2016-09-05 16:16:24 +0530

    Made the dropdown animatable.

--- src/sliders/DropdownSlider.js
+++ src/sliders/DropdownSlider.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class DropdownSlider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {draw: false, height: 0};
+    this.state = {};
     this.setSliderElement = this.setSliderElement.bind(this);
     this.drawSlider = this.drawSlider.bind(this);
   }
@@ -13,41 +13,46 @@ export default class DropdownSlider extends React.Component {
   }
 
   getHeight() {
+    // Scroll height gives the correct height even if we have
+    // provided a different height and/or have overflow: hidden instead
+    // of scroll
     return this.sliderElement ? this.sliderElement.scrollHeight: 0;
   }
 
   setSliderElement(sliderElement) {
     this.sliderElement = sliderElement;
+    this.setState({'fullHeight': this.getHeight()})
   }
 
   drawSlider() {
-    const height = this.getHeight();
     if (this.props.handleClick) {
       this.props.handleClick();
-    } else {
-      this.setState({
-        draw: !this.state.draw,
-        height: height
-      });
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // If menu is being closed Add the current fullHeight to state
+    if(nextProps.draw === false) {
+      this.setState({'fullHeight': this.getHeight()})
     }
   }
 
   render() {
     const {draw} = this.props;
-    const sliderTransform = draw === true ? '0' : '-100%';
+    // const sliderTransform = draw === true ? '0' : '-100%';
     const drawnClass = draw ? 'dropdown-slider--drawn': '';
     const styles = {
-      transform : `translateY(${sliderTransform})`
+      height : draw ? `${this.state.fullHeight}px` : 0
     };
 
     return (
-      <div role="button" aria-pressed={`${draw}`} aria-expanded={`${draw}`} aria-haspopup="true" onClick={this.drawSlider} ref={this.setSliderElement}className={`dropdown-slider ${drawnClass}`}>
+      <div role="button" aria-pressed={`${draw}`} aria-expanded={`${draw}`} aria-haspopup="true" onClick={this.drawSlider} className={`dropdown-slider ${drawnClass}`}>
         <a className="dropdown-slider__title" ref={(ref) => this.titleElement = ref} tabIndex="0">
           {this.props.title}
           <span className="dropdown-slider--caret"/>
         </a>
 
-        <div  style={styles} className="dropdown-slider--children">{this.props.children}</div>
+        <div ref={this.setSliderElement} style={styles} className="dropdown-slider--children">{this.props.children}</div>
       </div>
     );
   }
